f1ce907898cadddc4c8bec871b5bad20498ed0f8
reversonthaiam <70376757+reversonthaiam@users.noreply.github.com>
2021-01-25 11:56:15 -0300

    problema resolvido

--- frontend - redux/src/index.jsx
+++ frontend - redux/src/index.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+
 import App from "./main/app";
-import reducers from "./main/app";
+import reducers from "./main/reducers";
 
 const store = createStore(reducers);
 
--- frontend - redux/src/todo/todoList.jsx
+++ frontend - redux/src/todo/todoList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import IconButton from "../template/iconButton";
 
 const TodoList = (props) => {
